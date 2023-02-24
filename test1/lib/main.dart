5bafd4caf7c5311021b1613a0c13744adb9e6124
Logan J. Wang <59853630+lwdaolong@users.noreply.github.com>
2023-02-23 16:40:01 -0800

    Firebase implementation

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
